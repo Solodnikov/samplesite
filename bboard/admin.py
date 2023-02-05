f0b5601ed2a69178b590a40e1a5c1d4cfb32c501
Александр Солодников <solodnikov@yandex.ru>
2023-02-05 23:43:22 +0300

    Base

--- bboard/admin.py
+++ bboard/admin.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 
-from .models import Bb
+from .models import Bb, Rubric
 
 
 class BbAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'title', 'content', 'price', 'published')
+    list_display = ('pk', 'title', 'content', 'price','rubric', 'published')
     list_display_links = ('title', 'content')
     search_fields = ('title', 'content')
     empty_value_display = '-пусто-'
 
 
 admin.site.register(Bb, BbAdmin)
+admin.site.register(Rubric)
